ded1df9bd318a77fe120610ed0cbc7f0c0832b4f
freddyscs <freddy.rodriguez@smartmatic.com>
2020-06-08 17:48:15 +0200

    con dashboard

---
+++ client/src/components/dashboard/dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>DASHBOARD {{token}}
+        <button type="button" class="btn btn-lg btn-primary btn-block"  v-on:click="killToken()" >kill token</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+             token: localStorage.getItem('token') || null,
+          
+        }
+    },
+    methods:{
+        killToken: function(){
+        
+        this.token = localStorage.removeItem('token');
+      }
+    },
+    Props: {
+        
+    }
+}
+</script>
+<style scoped>
+
+</style>
